4f422dbda10fdf8d42ae77bb929a22439033b4bd
Yoshiya Hinosawa <stibium121@gmail.com>
2015-12-24 13:51:28 +0900

    Fix config load path for gulp-load-plugin

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-module.exports = function (gulp) {
+var path = require('path')
+
+/**
+ * @param {Gulp} gulp The gulp object
+ * @param {String|Object} [config] package.json path or package.json contents as object. defaults to this module's parent's package.json
+ */
+module.exports = function (gulp, config) {
     'use strict'
+
+    config = config || path.dirname(module.parent.filename) + '/package.json'
 
     var src = gulp.src
     var dest = gulp.dest
     var task = gulp.task.bind(gulp)
     var watch = gulp.watch.bind(gulp)
-    var $ = require('gulp-load-plugins')()
+    var $ = require('gulp-load-plugins')({config: config})
 
     var globals = function () {
 
